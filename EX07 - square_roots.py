6d9e1fbf41336c538a32dff4f96367f0fb62bca2
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2021-11-03 22:30:01 -0300

    Add files via upload

---
+++ EX07 - square_roots.py
@@ -0,0 +1,26 @@
+import math
+# receive values
+a = float(input("Enter 'a' value:"))
+b = float(input("Enter 'b' value:"))
+c = float(input("Enter 'c' value:"))
+
+# calculate delta
+delta = (b**2)-(4*a*c)
+
+# calculate square roots
+if delta == 0:
+    sqrt1 = (-b + math.sqrt(delta))/(2*a)
+    print("the solution for this equation is", sqrt1)
+else:
+    if delta < 0:
+        print("this equation has an imaginary root")
+    else:  
+            sqrt1 = (-b + math.sqrt(delta))/(2*a)
+            sqrt2 = (-b - math.sqrt(delta))/(2*a)
+            if sqrt1<sqrt2:   
+                print("the roots for this equation are {} and {}".format(sqrt1, sqrt2))
+            else:
+                print("the roots for this equation are {} and {}".format(sqrt2, sqrt1))   
+
+
+
